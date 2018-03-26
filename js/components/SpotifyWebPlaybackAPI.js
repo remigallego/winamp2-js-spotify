1bec41ef5dc6b62260ed92a244ef3fb1ed6612da
remi.gallego@gmail.com <remi.gallego@gmail.com>
2018-03-26 20:33:41 +0200

    improved debug UI

--- js/components/SpotifyWebPlaybackAPI.js
+++ js/components/SpotifyWebPlaybackAPI.js
@@ -13,6 +13,9 @@ class SpotifyWebPlaybackAPI extends React.Component {
 constructor(props) {
   super(props)
   this.handleScriptLoad = this.handleScriptLoad.bind(this)
+  this.state = {
+    input: "spotify:user:remifasol:playlist:43uS6JpETpdcZxTtK7cxCR"
+  }
 }
 
 handleScriptLoad()  {
@@ -25,23 +28,26 @@ handleScriptError(e) {
 
 }
 
+handleChange(e) {
+  e.preventDefault();
+  this.setState({input: e.target.value})
+}
 
 handleSubmit(e) {
   e.preventDefault()
   this.props.addTracksFromPlaylist(e.target[0].value)
 }
 
-
 render() {
   return(
     <div>
       {/*       DEBUG       */}
       <button onClick={() => this.props.removeAllTracks()}>Remove All Tracks</button>
-      <button onClick={() => this.props.addTrackFromURI("4rzfv0JLZfVhOhbSQ8o5jZ")}>Add Random Track</button>
-      <button onClick={() => this.props.addTrackFromURI("1ngKxzxHTfZ2l5IU3lq2V8")}>Add Random Track 2</button>
-      <button onClick={() => this.props.addTracksFromPlaylist("spotify:user:remifasol:playlist:43uS6JpETpdcZxTtK7cxCR")}>Add Tracks from Random Playlist</button>
+      <button onClick={() => this.props.addTrackFromURI("4rzfv0JLZfVhOhbSQ8o5jZ")}>Add Track Odiseo - Api</button>
+      <button onClick={() => this.props.addTrackFromURI("1ngKxzxHTfZ2l5IU3lq2V8")}>Add Track Meteor - Arctec Gemini</button>
+      <button onClick={() => this.props.addTracksFromPlaylist("spotify:user:remifasol:playlist:43uS6JpETpdcZxTtK7cxCR")}>Add Tracks from ./progandsynths playlist</button>
       <form onSubmit={(e) => this.handleSubmit(e)}>
-      <input value="spotify:user:remifasol:playlist:43uS6JpETpdcZxTtK7cxCR" readOnly style={{width: "700px"}}>
+      <input value={this.state.input} onChange={(e) => this.handleChange(e)} style={{width: "700px"}}>
       </input></form>
    </div>
   )
